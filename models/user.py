c731505900d9d99cb0ea0162abfd71034599ad23
agustinmeriles <agustinmeriles@hotmail.com>
2023-03-22 14:00:45 -0300

    user.py

--- models/user.py
+++ models/user.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, base
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy import create_engine
 
 
-class User(BaseModel, base):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
