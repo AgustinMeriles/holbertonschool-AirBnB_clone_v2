bfac5cf251c72901caa04a12d16b8e0942624298
Agus <agustinmeriles@hotmail.com>
2023-03-23 19:56:16 -0300

    task 6

--- models/state.py
+++ models/state.py
@@ -10,3 +10,13 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """Returns the list of cities associated with this state"""
+        from models import storage
+        cities = []
+        for city in storage.all("City").values():
+            if city.state_id == self.id:
+                cities.append(city)
+        return cities
