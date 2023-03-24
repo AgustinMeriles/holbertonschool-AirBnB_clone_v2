20bcbeddae5bcadf92c497b814701f87ccbd4c96
agustinmeriles <agustinmeriles@hotmail.com>
2023-03-24 09:42:12 -0300

    Fix

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key in "__class__":
