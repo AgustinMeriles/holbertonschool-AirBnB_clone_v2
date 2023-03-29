1f178e7ad66154542deb969a3077fcea020afbb0
Agus <agustinmeriles@hotmail.com>
2023-03-29 16:42:30 -0300

    5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Shebang"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    t = text.replace("_", " ")
+    return "C " + t
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py(text="is cool"):
+    text = text.replace("_", " ")
+    return "Python" + " " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def template(n):
+    return "<h1>Number: {}</h1>".format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
