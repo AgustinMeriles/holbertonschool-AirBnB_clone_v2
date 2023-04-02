243939ae3da2605583775006ed5798f103ba83ad
agustinmeriles <agustinmeriles@hotmail.com>
2023-04-01 22:15:41 -0300

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,3 +70,7 @@ class DBStorage:
         session = scoped_session(sessionmaker(
             bind=self.__engine, expire_on_commit=False))
         self.__session = session
+
+    def close(self):
+        """ Close method to call remove() method """
+        self.__session.close()
